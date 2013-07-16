d4a028d15813758d662b7b8052e8204c7480fda9
lincoln.liu <xianlinbox@gmail.com>
2013-07-16 18:27:48 +1000

    Initial Commit

---
+++ LighterViewControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LighterViewControllerDemo
+//
+//  Created by lincoln.liu on 7/16/13.
+//  Copyright (c) 2013 xianlinbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LighterViewControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LighterViewControllerDemo
+//
+//  Created by lincoln.liu on 7/16/13.
+//  Copyright (c) 2013 xianlinbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
